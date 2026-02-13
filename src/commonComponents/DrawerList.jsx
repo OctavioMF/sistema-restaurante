758daf170b196108631c457cfab03386ecef4220
Octa <octyfm18@gmail.com>
2026-02-13 20:08:57 -0300

    nuevo drawer

--- src/commonComponents/DrawerList.jsx
+++ src/commonComponents/DrawerList.jsx
@@ -9,54 +9,15 @@ import MailIcon from '@mui/icons-material/Mail';
 
 
 
-export default function DrawerList({open}) {
+export default function DrawerList() {
     return (<List>
-        {['Inbox', 'Starred', 'Send email'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-                <ListItemButton
-                    sx={[
-                        {
-                            minHeight: 48,
-                            px: 2.5,
-                        },
-                        open
-                            ? {
-                                justifyContent: 'initial',
-                            }
-                            : {
-                                justifyContent: 'center',
-                            },
-                    ]}
-                >
-                    <ListItemIcon
-                        sx={[
-                            {
-                                minWidth: 0,
-                                justifyContent: 'center',
-                            },
-                            open
-                                ? {
-                                    mr: 3,
-                                }
-                                : {
-                                    mr: 'auto',
-                                },
-                        ]}
-                    >
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+                <ListItemButton>
+                    <ListItemIcon>
                         {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                     </ListItemIcon>
-                    <ListItemText
-                        primary={text}
-                        sx={[
-                            open
-                                ? {
-                                    opacity: 1,
-                                }
-                                : {
-                                    opacity: 0,
-                                },
-                        ]}
-                    />
+                    <ListItemText primary={text} />
                 </ListItemButton>
             </ListItem>
         ))}
--- src/commonComponents/Layout.jsx
+++ src/commonComponents/Layout.jsx
@@ -12,140 +12,49 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import DrawerList from "./DrawerList";
+import ListItem from "@mui/material/ListItem";
+import List from "@mui/material/List";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import {AppBar, Drawer} from "@mui/material";
 
-const drawerWidth = 240;
+const drawerWidth = 180;
 
-const openedMixin = (theme) => ({
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: 'hidden',
-});
-
-const closedMixin = (theme) => ({
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up('sm')]: {
-        width: `calc(${theme.spacing(8)} + 1px)`,
-    },
-});
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-}));
-
-const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme }) => ({
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    variants: [
-        {
-            props: ({ open }) => open,
-            style: {
-                marginLeft: drawerWidth,
-                width: `calc(100% - ${drawerWidth}px)`,
-                transition: theme.transitions.create(['width', 'margin'], {
-                    easing: theme.transitions.easing.sharp,
-                    duration: theme.transitions.duration.enteringScreen,
-                }),
-            },
-        },
-    ],
-}));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme }) => ({
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-        boxSizing: 'border-box',
-        variants: [
-            {
-                props: ({ open }) => open,
-                style: {
-                    ...openedMixin(theme),
-                    '& .MuiDrawer-paper': openedMixin(theme),
-                },
-            },
-            {
-                props: ({ open }) => !open,
-                style: {
-                    ...closedMixin(theme),
-                    '& .MuiDrawer-paper': closedMixin(theme),
-                },
-            },
-        ],
-    }),
-);
-
-export default function MiniDrawer({children}) {
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
-
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
-
+export default function Layout({children}) {
     return (
-        <Box sx={{ display: 'flex' , height: '100vh' , overflowY: 'hidden' }}>
-            <CssBaseline />
-            <AppBar position="fixed" open={open}>
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        sx={[
-                            {
-                                marginRight: 5,
-                            },
-                            open && { display: 'none' },
-                        ]}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h5" noWrap component="div">
-                        Stock
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Drawer variant="permanent" open={open}>
-                <DrawerHeader>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-                    </IconButton>
-                </DrawerHeader>
+        <Box sx={{ display: 'flex', height:'100vh', backgroundColor: '#1e1e1e' }}>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                        backgroundColor: '#1e1e1e',
+                        color: 'white',
+                        borderRight: '1px solid #333',
+                        '& .MuiSvgIcon-root': {
+                            color: 'white'
+                        }
+                    },
+                }}
+                variant="permanent"
+                anchor="left"
+            >
+                <Toolbar />
                 <Divider />
-                <DrawerList open={open}></DrawerList>
+                <DrawerList></DrawerList>
             </Drawer>
             <Box component="main" sx={{
                 flexGrow: 1,
-                height: '100vh',
+                height: '100%',
                 overflow: 'hidden',
                 display: 'flex',
                 flexDirection: 'column',
                 p: 0}}>
-                <DrawerHeader />
                 {children}
             </Box>
         </Box>
